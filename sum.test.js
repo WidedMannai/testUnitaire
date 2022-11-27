cff09f9333cd22e00a5096beb4773bdc3c9ff8df
MANNAI Wided <72729292+WidedMannai@users.noreply.github.com>
2022-11-27 23:52:45 +0100

    Add files via upload

---
+++ sum.test.js
@@ -0,0 +1,13 @@
+const sum = require("./sum")
+
+describe(
+    'test sum', ()=>{
+        test('test 1 + 2 = 3', ()=>{
+            expect(sum(1,2)).toBe(3)
+        })
+
+        test('test -1 + -2 = -3', ()=>{
+            expect(sum(1,2)).toBe(-3)
+        })
+    }
+     )
